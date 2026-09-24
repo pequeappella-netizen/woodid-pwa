68d7151d63bf04e4936723bc96ed050b275a33fd
pequeappella-netizen <pequeappella@hotmail.com>
2026-09-24 17:46:25 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='woodid-09';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
